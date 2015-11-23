345e884da1b2c9221ccacf64b23eeff1698b0bc4
mmalone136 <mmalone136@gmail.com>
2015-11-23 23:07:17 +0000

    ?

---
+++ MALT Music/HomePage.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace MALT_Music
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Font = new System.Drawing.Font("Source Sans Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFullName.Location = new System.Drawing.Point(710, 9);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(57, 24);
+            this.lblFullName.TabIndex = 0;
+            this.lblFullName.Text = "Name";
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 434);
+            this.Controls.Add(this.lblFullName);
+            this.Name = "HomePage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.HomePage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFullName;
+
+    }
+}
+
---
+++ MALT Music/HomePage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cassandra;
+using MALT_Music.DataObjects;
+
+namespace MALT_Music
+{
+    public partial class HomePage : Form
+    {
+
+        public static User currentUser;
+
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void HomePage_Load(object sender, EventArgs e)
+        {
+            lblFullName.Text = currentUser.getFirstName() + " " + currentUser.getLastName();
+        }
+
+    }
+}
