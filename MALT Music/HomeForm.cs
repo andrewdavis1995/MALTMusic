01b8921928413e7e540ac2395e995881b909118c
mmalone136 <mmalone136@gmail.com>
2015-11-23 16:38:22 +0000

    Keyspace Adding started

--- MALT Music/HomeForm.cs
+++ MALT Music/HomeForm.cs
@@ -25,13 +25,33 @@ namespace MALT_Music
             Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
             ISession session = cluster.Connect("maltmusic");
 
-            RowSet rows = session.Execute("select * from maltmusic.userprofiles");
+
+            //var statement2 = session.Prepare("SELECT * FROM DynamicTimeUUIDTable WHERE id = :RowKey AND ColumnName IN :names");
+            var statement1 = session.Prepare("Select * from userprofiles where user_id = :RowKey");
+                
+                //List<string> nameslist = new List<string>();
+                //nameslist.Add("malt");
+                //nameslist.Add("colName2");
+
+            //RowSet rows = session.Execute(statement1.Bind(new { RowKey = "userseven", names = nameslist }));
+            RowSet rows = session.Execute(statement1.Bind(new { RowKey = "userseveneight" }));
+            //System.Console.WriteLine ("Here we are. The count is: ");
+                //System.Console.WriteLine (results2.Count());
+
+                foreach (Row row in rows)
+                {
+                    String lsString = row["first_name"] + "  " + row["last_name"] + " | " + row["user_id"];
+                    listBox1.Items.Add(lsString);
+                }
+
+            /*
+            RowSet rows = session.Execute("select * from userprofiles");
             foreach (Row row in rows)
             {
                 String lsString = row["first_name"] + " " + row["last_name"] + " | " + row ["user_id"];
                 listBox1.Items.Add(lsString);
             }
-
+            */
             //Wait for enter key before exiting
             Console.ReadLine();
         }
