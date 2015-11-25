62bdc650c88d9afe76bba1ad88457e05e2e76cc5
Auctioneer <ljgdavie@hotmail.com>
2015-11-25 16:27:46 +0000

    Better formatting for main page.

--- MALT Music/HomePage.Designer.cs
+++ MALT Music/HomePage.Designer.cs
@@ -31,23 +31,29 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
             this.lblFullName = new System.Windows.Forms.Label();
             this.lblBrowse = new System.Windows.Forms.Label();
-            this.picBoxBrowse = new System.Windows.Forms.PictureBox();
             this.lblPlaylist = new System.Windows.Forms.Label();
-            this.picBoxPlaylist = new System.Windows.Forms.PictureBox();
-            this.picBoxMALTLogo = new System.Windows.Forms.PictureBox();
-            this.picBoxMagnifyingGlass = new System.Windows.Forms.PictureBox();
             this.txtSearchBox = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.picBoxBrowse)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlaylist)).BeginInit();
+            this.picBoxMALTLogo = new System.Windows.Forms.PictureBox();
+            this.picBoxBrowse = new System.Windows.Forms.PictureBox();
+            this.picBoxMagnifyingGlass = new System.Windows.Forms.PictureBox();
+            this.picBoxPlaylist = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxMALTLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxBrowse)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxMagnifyingGlass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlaylist)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblFullName
             // 
             this.lblFullName.AutoSize = true;
             this.lblFullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFullName.Location = new System.Drawing.Point(485, 258);
+            this.lblFullName.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblFullName.Location = new System.Drawing.Point(82, 4);
             this.lblFullName.Name = "lblFullName";
             this.lblFullName.Size = new System.Drawing.Size(61, 24);
             this.lblFullName.TabIndex = 0;
@@ -58,97 +64,134 @@
             // 
             this.lblBrowse.AutoSize = true;
             this.lblBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBrowse.Location = new System.Drawing.Point(363, 320);
+            this.lblBrowse.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblBrowse.Location = new System.Drawing.Point(82, 241);
             this.lblBrowse.Name = "lblBrowse";
             this.lblBrowse.Size = new System.Drawing.Size(62, 20);
             this.lblBrowse.TabIndex = 4;
             this.lblBrowse.Text = "Browse";
             // 
+            // lblPlaylist
+            // 
+            this.lblPlaylist.AutoSize = true;
+            this.lblPlaylist.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlaylist.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblPlaylist.Location = new System.Drawing.Point(82, 118);
+            this.lblPlaylist.Name = "lblPlaylist";
+            this.lblPlaylist.Size = new System.Drawing.Size(65, 20);
+            this.lblPlaylist.TabIndex = 2;
+            this.lblPlaylist.Text = "Playlists";
+            // 
+            // txtSearchBox
+            // 
+            this.txtSearchBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtSearchBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSearchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchBox.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.txtSearchBox.Location = new System.Drawing.Point(384, 47);
+            this.txtSearchBox.Name = "txtSearchBox";
+            this.txtSearchBox.Size = new System.Drawing.Size(539, 33);
+            this.txtSearchBox.TabIndex = 0;
+            this.txtSearchBox.Text = "Search for songs, artists and albums...";
+            this.txtSearchBox.Enter += new System.EventHandler(this.txtSearchBox_Enter);
+            this.txtSearchBox.Leave += new System.EventHandler(this.txtSearchBox_Leave);
+            // 
+            // picBoxMALTLogo
+            // 
+            this.picBoxMALTLogo.Image = global::MALT_Music.Properties.Resources.logo;
+            this.picBoxMALTLogo.Location = new System.Drawing.Point(54, 16);
+            this.picBoxMALTLogo.Name = "picBoxMALTLogo";
+            this.picBoxMALTLogo.Size = new System.Drawing.Size(176, 89);
+            this.picBoxMALTLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxMALTLogo.TabIndex = 2;
+            this.picBoxMALTLogo.TabStop = false;
+            // 
             // picBoxBrowse
             // 
-            this.picBoxBrowse.Image = global::MALT_Music.Properties.Resources.Music_Treble_Clef_icon;
-            this.picBoxBrowse.Location = new System.Drawing.Point(670, 242);
+            this.picBoxBrowse.Image = global::MALT_Music.Properties.Resources.Music_icon_white;
+            this.picBoxBrowse.Location = new System.Drawing.Point(74, 167);
             this.picBoxBrowse.Name = "picBoxBrowse";
             this.picBoxBrowse.Size = new System.Drawing.Size(81, 72);
             this.picBoxBrowse.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBoxBrowse.TabIndex = 3;
             this.picBoxBrowse.TabStop = false;
             // 
-            // lblPlaylist
-            // 
-            this.lblPlaylist.AutoSize = true;
-            this.lblPlaylist.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlaylist.Location = new System.Drawing.Point(244, 224);
-            this.lblPlaylist.Name = "lblPlaylist";
-            this.lblPlaylist.Size = new System.Drawing.Size(65, 20);
-            this.lblPlaylist.TabIndex = 2;
-            this.lblPlaylist.Text = "Playlists";
-            // 
-            // picBoxPlaylist
-            // 
-            this.picBoxPlaylist.Image = global::MALT_Music.Properties.Resources.List;
-            this.picBoxPlaylist.Location = new System.Drawing.Point(315, 224);
-            this.picBoxPlaylist.Name = "picBoxPlaylist";
-            this.picBoxPlaylist.Size = new System.Drawing.Size(81, 72);
-            this.picBoxPlaylist.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picBoxPlaylist.TabIndex = 1;
-            this.picBoxPlaylist.TabStop = false;
-            // 
-            // picBoxMALTLogo
-            // 
-            this.picBoxMALTLogo.Image = global::MALT_Music.Properties.Resources.logo;
-            this.picBoxMALTLogo.Location = new System.Drawing.Point(345, 73);
-            this.picBoxMALTLogo.Name = "picBoxMALTLogo";
-            this.picBoxMALTLogo.Size = new System.Drawing.Size(125, 62);
-            this.picBoxMALTLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picBoxMALTLogo.TabIndex = 2;
-            this.picBoxMALTLogo.TabStop = false;
-            // 
             // picBoxMagnifyingGlass
             // 
-            this.picBoxMagnifyingGlass.Image = global::MALT_Music.Properties.Resources.Magnifying_glass_icon;
+            this.picBoxMagnifyingGlass.Image = global::MALT_Music.Properties.Resources.Magnifying_glass_icon_white;
             this.picBoxMagnifyingGlass.InitialImage = ((System.Drawing.Image)(resources.GetObject("picBoxMagnifyingGlass.InitialImage")));
-            this.picBoxMagnifyingGlass.Location = new System.Drawing.Point(217, 114);
+            this.picBoxMagnifyingGlass.Location = new System.Drawing.Point(324, 41);
             this.picBoxMagnifyingGlass.Name = "picBoxMagnifyingGlass";
             this.picBoxMagnifyingGlass.Size = new System.Drawing.Size(50, 47);
             this.picBoxMagnifyingGlass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBoxMagnifyingGlass.TabIndex = 1;
             this.picBoxMagnifyingGlass.TabStop = false;
             // 
-            // txtSearchBox
+            // picBoxPlaylist
             // 
-            this.txtSearchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearchBox.Location = new System.Drawing.Point(336, 158);
-            this.txtSearchBox.Name = "txtSearchBox";
-            this.txtSearchBox.Size = new System.Drawing.Size(539, 40);
-            this.txtSearchBox.TabIndex = 0;
-            this.txtSearchBox.Text = "Search for songs, artists and albums...";
-            this.txtSearchBox.Enter += new System.EventHandler(this.txtSearchBox_Enter);
-            this.txtSearchBox.Leave += new System.EventHandler(this.txtSearchBox_Leave);
+            this.picBoxPlaylist.Image = global::MALT_Music.Properties.Resources.List_white;
+            this.picBoxPlaylist.Location = new System.Drawing.Point(74, 43);
+            this.picBoxPlaylist.Name = "picBoxPlaylist";
+            this.picBoxPlaylist.Size = new System.Drawing.Size(81, 72);
+            this.picBoxPlaylist.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxPlaylist.TabIndex = 1;
+            this.picBoxPlaylist.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.picBoxMagnifyingGlass);
+            this.panel1.Controls.Add(this.txtSearchBox);
+            this.panel1.Controls.Add(this.picBoxMALTLogo);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1113, 125);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Controls.Add(this.lblFullName);
+            this.panel2.Controls.Add(this.picBoxPlaylist);
+            this.panel2.Controls.Add(this.lblBrowse);
+            this.panel2.Controls.Add(this.lblPlaylist);
+            this.panel2.Controls.Add(this.picBoxBrowse);
+            this.panel2.Location = new System.Drawing.Point(-3, 124);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(233, 336);
+            this.panel2.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(930, 47);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(48, 33);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "GO";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
             // HomePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1048, 586);
-            this.Controls.Add(this.lblPlaylist);
-            this.Controls.Add(this.lblBrowse);
-            this.Controls.Add(this.picBoxMALTLogo);
-            this.Controls.Add(this.picBoxBrowse);
-            this.Controls.Add(this.picBoxMagnifyingGlass);
-            this.Controls.Add(this.lblFullName);
-            this.Controls.Add(this.picBoxPlaylist);
-            this.Controls.Add(this.txtSearchBox);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.IsMdiContainer = true;
             this.Name = "HomePage";
             this.Text = "MALT Music";
             this.Load += new System.EventHandler(this.HomePage_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.picBoxBrowse)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlaylist)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxMALTLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxBrowse)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxMagnifyingGlass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPlaylist)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -162,6 +205,9 @@
         private System.Windows.Forms.PictureBox picBoxBrowse;
         private System.Windows.Forms.Label lblPlaylist;
         private System.Windows.Forms.Label lblBrowse;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSearch;
 
     }
 }
