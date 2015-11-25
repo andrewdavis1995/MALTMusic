6e7f474648645e12cdf5953d0e3d6b74d65c79ce
Budds2k6 <Budds_2k6@hotmail.co.uk>
2015-11-25 21:27:15 +0000

    Added ability to change current position of track play - WIP. Tweaked interface slightly - WIP. Crumpets soon - WIP.

--- MALT Music/frmMusicPlayer.Designer.cs
+++ MALT Music/frmMusicPlayer.Designer.cs
@@ -36,16 +36,21 @@
             this.lblFileName = new System.Windows.Forms.Label();
             this.ofdMusicOpener = new System.Windows.Forms.OpenFileDialog();
             this.picBoxAlbumArt = new System.Windows.Forms.PictureBox();
-            this.prbTrackBar = new System.Windows.Forms.ProgressBar();
             this.tmrTracker = new System.Windows.Forms.Timer(this.components);
             this.btnPause = new System.Windows.Forms.Button();
             this.grpRepeat = new System.Windows.Forms.GroupBox();
-            this.rbnNone = new System.Windows.Forms.RadioButton();
-            this.rbnCurrent = new System.Windows.Forms.RadioButton();
-            this.rbnOnce = new System.Windows.Forms.RadioButton();
             this.rbnPlaylist = new System.Windows.Forms.RadioButton();
+            this.rbnOnce = new System.Windows.Forms.RadioButton();
+            this.rbnCurrent = new System.Windows.Forms.RadioButton();
+            this.rbnNone = new System.Windows.Forms.RadioButton();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTimeOne = new System.Windows.Forms.Label();
+            this.lblTimeTwo = new System.Windows.Forms.Label();
+            this.trbTrackSelect = new System.Windows.Forms.TrackBar();
+            this.prbTrackBar = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxAlbumArt)).BeginInit();
             this.grpRepeat.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbTrackSelect)).BeginInit();
             this.SuspendLayout();
             // 
             // btnPlay
@@ -54,9 +59,9 @@
             this.btnPlay.Enabled = false;
             this.btnPlay.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPlay.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnPlay.Location = new System.Drawing.Point(225, 93);
+            this.btnPlay.Location = new System.Drawing.Point(265, 12);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(93, 24);
+            this.btnPlay.Size = new System.Drawing.Size(108, 26);
             this.btnPlay.TabIndex = 0;
             this.btnPlay.Text = "Play";
             this.btnPlay.UseVisualStyleBackColor = false;
@@ -67,9 +72,9 @@
             this.btnStop.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.btnStop.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStop.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnStop.Location = new System.Drawing.Point(423, 93);
+            this.btnStop.Location = new System.Drawing.Point(493, 13);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(93, 24);
+            this.btnStop.Size = new System.Drawing.Size(108, 26);
             this.btnStop.TabIndex = 1;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = false;
@@ -80,9 +85,9 @@
             this.btnTest.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.btnTest.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTest.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnTest.Location = new System.Drawing.Point(324, 12);
+            this.btnTest.Location = new System.Drawing.Point(151, 41);
             this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(93, 24);
+            this.btnTest.Size = new System.Drawing.Size(108, 26);
             this.btnTest.TabIndex = 2;
             this.btnTest.Text = "Load Test File";
             this.btnTest.UseVisualStyleBackColor = false;
@@ -93,9 +98,9 @@
             this.btnOpen.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.btnOpen.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOpen.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnOpen.Location = new System.Drawing.Point(225, 12);
+            this.btnOpen.Location = new System.Drawing.Point(151, 12);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(93, 24);
+            this.btnOpen.Size = new System.Drawing.Size(108, 26);
             this.btnOpen.TabIndex = 3;
             this.btnOpen.Text = "Open";
             this.btnOpen.UseVisualStyleBackColor = false;
@@ -103,14 +108,16 @@
             // 
             // lblFileName
             // 
-            this.lblFileName.AutoSize = true;
+            this.lblFileName.AutoEllipsis = true;
             this.lblFileName.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lblFileName.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFileName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblFileName.Location = new System.Drawing.Point(222, 77);
+            this.lblFileName.Location = new System.Drawing.Point(367, 52);
             this.lblFileName.Name = "lblFileName";
-            this.lblFileName.Size = new System.Drawing.Size(0, 14);
+            this.lblFileName.Size = new System.Drawing.Size(350, 15);
             this.lblFileName.TabIndex = 4;
+            this.lblFileName.Text = "No file selected";
             // 
             // ofdMusicOpener
             // 
@@ -123,20 +130,12 @@
             this.picBoxAlbumArt.ErrorImage = global::MALT_Music.Properties.Resources.logo;
             this.picBoxAlbumArt.Image = global::MALT_Music.Properties.Resources.logo;
             this.picBoxAlbumArt.InitialImage = global::MALT_Music.Properties.Resources.logo;
-            this.picBoxAlbumArt.Location = new System.Drawing.Point(108, 12);
+            this.picBoxAlbumArt.Location = new System.Drawing.Point(12, 12);
             this.picBoxAlbumArt.Name = "picBoxAlbumArt";
-            this.picBoxAlbumArt.Size = new System.Drawing.Size(111, 105);
+            this.picBoxAlbumArt.Size = new System.Drawing.Size(129, 113);
             this.picBoxAlbumArt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBoxAlbumArt.TabIndex = 5;
             this.picBoxAlbumArt.TabStop = false;
-            // 
-            // prbTrackBar
-            // 
-            this.prbTrackBar.Location = new System.Drawing.Point(225, 64);
-            this.prbTrackBar.Name = "prbTrackBar";
-            this.prbTrackBar.Size = new System.Drawing.Size(390, 10);
-            this.prbTrackBar.Step = 1;
-            this.prbTrackBar.TabIndex = 6;
             // 
             // tmrTracker
             // 
@@ -149,9 +148,9 @@
             this.btnPause.Enabled = false;
             this.btnPause.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPause.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnPause.Location = new System.Drawing.Point(324, 94);
+            this.btnPause.Location = new System.Drawing.Point(379, 13);
             this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(93, 23);
+            this.btnPause.Size = new System.Drawing.Size(108, 25);
             this.btnPause.TabIndex = 7;
             this.btnPause.Text = "Pause";
             this.btnPause.UseVisualStyleBackColor = false;
@@ -165,51 +164,17 @@
             this.grpRepeat.Controls.Add(this.rbnNone);
             this.grpRepeat.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grpRepeat.ForeColor = System.Drawing.Color.White;
-            this.grpRepeat.Location = new System.Drawing.Point(526, 76);
+            this.grpRepeat.Location = new System.Drawing.Point(734, 12);
             this.grpRepeat.Name = "grpRepeat";
-            this.grpRepeat.Size = new System.Drawing.Size(286, 41);
+            this.grpRepeat.Size = new System.Drawing.Size(334, 44);
             this.grpRepeat.TabIndex = 8;
             this.grpRepeat.TabStop = false;
             this.grpRepeat.Text = "Repeat Options";
             // 
-            // rbnNone
-            // 
-            this.rbnNone.AutoSize = true;
-            this.rbnNone.Checked = true;
-            this.rbnNone.Location = new System.Drawing.Point(6, 19);
-            this.rbnNone.Name = "rbnNone";
-            this.rbnNone.Size = new System.Drawing.Size(53, 18);
-            this.rbnNone.TabIndex = 0;
-            this.rbnNone.TabStop = true;
-            this.rbnNone.Text = "None";
-            this.rbnNone.UseVisualStyleBackColor = true;
-            // 
-            // rbnCurrent
-            // 
-            this.rbnCurrent.AutoSize = true;
-            this.rbnCurrent.Location = new System.Drawing.Point(124, 17);
-            this.rbnCurrent.Name = "rbnCurrent";
-            this.rbnCurrent.Size = new System.Drawing.Size(74, 18);
-            this.rbnCurrent.TabIndex = 1;
-            this.rbnCurrent.TabStop = true;
-            this.rbnCurrent.Text = "Current";
-            this.rbnCurrent.UseVisualStyleBackColor = true;
-            // 
-            // rbnOnce
-            // 
-            this.rbnOnce.AutoSize = true;
-            this.rbnOnce.Location = new System.Drawing.Point(65, 17);
-            this.rbnOnce.Name = "rbnOnce";
-            this.rbnOnce.Size = new System.Drawing.Size(53, 18);
-            this.rbnOnce.TabIndex = 2;
-            this.rbnOnce.TabStop = true;
-            this.rbnOnce.Text = "Once";
-            this.rbnOnce.UseVisualStyleBackColor = true;
-            // 
             // rbnPlaylist
             // 
             this.rbnPlaylist.AutoSize = true;
-            this.rbnPlaylist.Location = new System.Drawing.Point(204, 17);
+            this.rbnPlaylist.Location = new System.Drawing.Point(238, 18);
             this.rbnPlaylist.Name = "rbnPlaylist";
             this.rbnPlaylist.Size = new System.Drawing.Size(81, 18);
             this.rbnPlaylist.TabIndex = 3;
@@ -217,31 +182,118 @@
             this.rbnPlaylist.Text = "Playlist";
             this.rbnPlaylist.UseVisualStyleBackColor = true;
             // 
+            // rbnOnce
+            // 
+            this.rbnOnce.AutoSize = true;
+            this.rbnOnce.Location = new System.Drawing.Point(76, 18);
+            this.rbnOnce.Name = "rbnOnce";
+            this.rbnOnce.Size = new System.Drawing.Size(53, 18);
+            this.rbnOnce.TabIndex = 2;
+            this.rbnOnce.TabStop = true;
+            this.rbnOnce.Text = "Once";
+            this.rbnOnce.UseVisualStyleBackColor = true;
+            // 
+            // rbnCurrent
+            // 
+            this.rbnCurrent.AutoSize = true;
+            this.rbnCurrent.Location = new System.Drawing.Point(145, 18);
+            this.rbnCurrent.Name = "rbnCurrent";
+            this.rbnCurrent.Size = new System.Drawing.Size(74, 18);
+            this.rbnCurrent.TabIndex = 1;
+            this.rbnCurrent.TabStop = true;
+            this.rbnCurrent.Text = "Current";
+            this.rbnCurrent.UseVisualStyleBackColor = true;
+            // 
+            // rbnNone
+            // 
+            this.rbnNone.AutoSize = true;
+            this.rbnNone.Checked = true;
+            this.rbnNone.Location = new System.Drawing.Point(7, 20);
+            this.rbnNone.Name = "rbnNone";
+            this.rbnNone.Size = new System.Drawing.Size(53, 18);
+            this.rbnNone.TabIndex = 0;
+            this.rbnNone.TabStop = true;
+            this.rbnNone.Text = "None";
+            this.rbnNone.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(261, 52);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(105, 14);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Player Status:";
+            // 
+            // lblTimeOne
+            // 
+            this.lblTimeOne.AutoSize = true;
+            this.lblTimeOne.Location = new System.Drawing.Point(261, 66);
+            this.lblTimeOne.Name = "lblTimeOne";
+            this.lblTimeOne.Size = new System.Drawing.Size(49, 14);
+            this.lblTimeOne.TabIndex = 10;
+            this.lblTimeOne.Text = "label1";
+            // 
+            // lblTimeTwo
+            // 
+            this.lblTimeTwo.AutoSize = true;
+            this.lblTimeTwo.Location = new System.Drawing.Point(668, 67);
+            this.lblTimeTwo.Name = "lblTimeTwo";
+            this.lblTimeTwo.Size = new System.Drawing.Size(49, 14);
+            this.lblTimeTwo.TabIndex = 11;
+            this.lblTimeTwo.Text = "label2";
+            // 
+            // trbTrackSelect
+            // 
+            this.trbTrackSelect.Location = new System.Drawing.Point(147, 80);
+            this.trbTrackSelect.Name = "trbTrackSelect";
+            this.trbTrackSelect.Size = new System.Drawing.Size(917, 45);
+            this.trbTrackSelect.TabIndex = 12;
+            this.trbTrackSelect.Value = 10;
+            this.trbTrackSelect.Scroll += new System.EventHandler(this.trbTrackSelect_Scroll);
+            // 
+            // prbTrackBar
+            // 
+            this.prbTrackBar.Location = new System.Drawing.Point(160, 102);
+            this.prbTrackBar.Name = "prbTrackBar";
+            this.prbTrackBar.Size = new System.Drawing.Size(890, 12);
+            this.prbTrackBar.Step = 1;
+            this.prbTrackBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.prbTrackBar.TabIndex = 13;
+            // 
             // frmMusicPlayer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(927, 127);
+            this.ClientSize = new System.Drawing.Size(1081, 137);
             this.ControlBox = false;
+            this.Controls.Add(this.prbTrackBar);
+            this.Controls.Add(this.trbTrackSelect);
+            this.Controls.Add(this.lblTimeTwo);
+            this.Controls.Add(this.lblTimeOne);
+            this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.grpRepeat);
             this.Controls.Add(this.btnPause);
-            this.Controls.Add(this.prbTrackBar);
             this.Controls.Add(this.picBoxAlbumArt);
             this.Controls.Add(this.lblFileName);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.btnTest);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnPlay);
-            this.MaximumSize = new System.Drawing.Size(943, 166);
-            this.MinimumSize = new System.Drawing.Size(943, 166);
+            this.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.MaximumSize = new System.Drawing.Size(1097, 176);
+            this.MinimumSize = new System.Drawing.Size(1097, 176);
             this.Name = "frmMusicPlayer";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MusicPlayer";
+            this.Load += new System.EventHandler(this.frmMusicPlayer_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picBoxAlbumArt)).EndInit();
             this.grpRepeat.ResumeLayout(false);
             this.grpRepeat.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbTrackSelect)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -256,7 +308,6 @@
         private System.Windows.Forms.Label lblFileName;
         private System.Windows.Forms.OpenFileDialog ofdMusicOpener;
         private System.Windows.Forms.PictureBox picBoxAlbumArt;
-        private System.Windows.Forms.ProgressBar prbTrackBar;
         private System.Windows.Forms.Timer tmrTracker;
         private System.Windows.Forms.Button btnPause;
         private System.Windows.Forms.GroupBox grpRepeat;
@@ -264,5 +315,10 @@
         private System.Windows.Forms.RadioButton rbnOnce;
         private System.Windows.Forms.RadioButton rbnCurrent;
         private System.Windows.Forms.RadioButton rbnNone;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTimeOne;
+        private System.Windows.Forms.Label lblTimeTwo;
+        private System.Windows.Forms.TrackBar trbTrackSelect;
+        private System.Windows.Forms.ProgressBar prbTrackBar;
     }
 }
