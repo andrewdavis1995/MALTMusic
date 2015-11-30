12df1901d6995c78b86d50497c7c7aaa60b93fc5
mmalone136 <mmalone136@gmail.com>
2015-11-30 12:47:17 +0000

    Go go gadget, get votes for a user

---
+++ MALT Music/DataObjects/UserVote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MALT_Music.DataObjects
+{
+    class UserVote
+    {
+        Guid tid;
+        // 1 up, -1 down
+        int howVoted;
+
+        public UserVote() { }
+
+        public UserVote(Guid id, int how) { this.tid = id; this.howVoted = how;}
+
+        public Guid getID() { return this.tid; }
+        public int getHowVoted() { return this.howVoted; }
+
+        public void setID(Guid id) { this.tid = id; }
+        public void setHowVoted(int x) { this.howVoted = x; }
+    }
+}
